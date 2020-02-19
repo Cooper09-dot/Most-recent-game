e07dcbc9c804c4ccdcdfca55c5ec7d6329744e51
Jermaine09 <cooper906@morrisville.edu>
2020-02-19 15:41:36 -0500

    Feb 19, 2020

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smooothSpeed = 0125f;
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
