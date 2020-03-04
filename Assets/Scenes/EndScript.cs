e2c2b3e14296bec1c02257e5f4de257d17f2518b
Jermaine09 <cooper906@morrisville.edu>
2020-03-04 09:36:52 -0500

    Most recent

---
+++ Assets/Scenes/EndScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (currentTime = 0)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EyeLook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLook : MonoBehaviour
+{
+
+    //mouse direction
+    private Vector2 mD;
+
+    //private Transform myBody;
+    void Start()
+    {
+       // myBody = this.transform.parent.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If you want to see the mouse again
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        float movement = Input.GetAxis("Vertical");
+        movement *= Time.deltaTime; 
+
+
+        //The range the mouse has moved up or down
+        Vector2 mC = new Vector2
+            (Input.GetAxisRaw("Mouse X") * 3f,
+                Input.GetAxisRaw("Mouse Y") * 3f);
+
+        mD += mC;
+          
+        
+
+        //myBody.localRotation =
+        //Quaternion.AngleAxis(mD.x, Vector3.up);
+
+        Quaternion qR = 
+        this.transform.localRotation =
+            Quaternion.AngleAxis(mD.x, Vector3.up);
+        
+        //The actual rotation
+        this.transform.localRotation = qR * 
+            Quaternion.AngleAxis(-mD.y, Vector3.right);
+
+        this.transform.Translate
+        (Vector3.forward * movement); 
+    }
+}
